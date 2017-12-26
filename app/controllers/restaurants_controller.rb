79c16721f3179c7daebbaa7eecaa34dbb443e9d9
Kevin Ho <johnwater007@gmail.com>
2017-12-26 14:00:10 +0800

    for s5 q2

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,46 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.page(params[:page]).per(9)
+    @categories = Category.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @comment = Comment.new
+  end
+
+  # GET restaurants/feeds
+  # 會去 render app/views/restuarants/feeds.html.erb
+  def feeds
+    @recent_restaurants = Restaurant.all.order(created_at: :desc).limit(10)
+    @recent_comments = Comment.all.order(created_at: :desc).limit(10)
+  end
+
+  # GET restaurants/dashboard
+  # 會去 render app/views/restuarants/dashboard.html.erb
+  def dashboard
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # POST /restaurants/:id/favorite
+  def favorite
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.favorites.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  # POST /restaurants/:id/unfavorite
+  def unfavorite
+    @restaurant = Restaurant.find(params[:id])
+
+    favorites = Favorite.where(restaurant: @restaurant, user: current_user)
+    favorites.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
+  # GET /restaurants/ranking
+  def ranking
+    @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
+end
